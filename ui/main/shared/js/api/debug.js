e23c555e0caf2b6878d4ec3482e53ed2664ff13c
Benjamin Rianda <reptarforlife@gmail.com>
2014-09-22 18:51:24 -0700

    Deleted unneeded libaries

--- ui/main/shared/js/api/debug.js
+++
@@ -1,8 +0,0 @@
-function init_debug(api) {
-
-    api.debug = {
-        abort: function () { engine.call('console_exec', 'debug_abort'); },
-        crash: function () { engine.call('console_exec', 'debug_crash'); }
-    };
-}
-init_debug(window.api);
--- ui/main/shared/js/api/youtube.js
+++
@@ -1,8 +0,0 @@
-function init_youtube(api) {
-    api.youtube = {
- 
-        // Open the YouTube page in an external browser
-        launchPage: function (videoId) { engine.call('youtube.launchPage', videoId); }
-    };
-};
-init_youtube(window.api);
